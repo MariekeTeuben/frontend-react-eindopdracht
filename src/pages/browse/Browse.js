03eb1a009592e5c4154cd426e44b8f22b2403320
MariekeTeuben <marieke@marenmaa.nl>
2022-11-13 14:35:01 +0100

    ul van parknamen

--- src/pages/browse/Browse.js
+++ src/pages/browse/Browse.js
@@ -1,38 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './Browse.css';
 import axios from "axios";
 
-function Browse() {
-    const [parkCards, setParkCards] = useState({});
 
+function Browse() {
+    const [parkCards, setParkCards] = useState([]);
+
+    useEffect(() => {
         async function fetchData() {
             try {
-                const result = await axios.get(`https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=gF5KCU6HVDRDuaDyTQuyKS6YOzzaNBkgJl1IlOZg`);
-                console.log(result.data);
-                setParkCards(result.data);
+                const result = await axios.get(`https://developer.nps.gov/api/v1/parks?&api_key=gF5KCU6HVDRDuaDyTQuyKS6YOzzaNBkgJl1IlOZg`);
+                console.log(result.data.data);
+                setParkCards(result.data.data);
             } catch (e) {
                 console.error(e);
             }
         }
 
+        fetchData();
+    }, []);
+
+
+
     return (
         <>
             <div>
                 <h1 className="black">Dit wordt de browsen pagina</h1>
-                <button
-                    type="button"
-                    onClick={fetchData}
-                    >
-                Haal data op!
-                </button>
+
                 <div>
-                    {Object.keys(parkCards).length > 0 &&
-                    <>
-                        <img src={parkCards.data[0].images[0].url} alt="foto"/>
-                        <h2>{parkCards.data[0].fullName}</h2>
-                        <h4>{parkCards.data[0].description}</h4>
-                    </>
-                    }
+                        <ul>
+                            {parkCards.length > 0 && parkCards.map((parkCard) => {
+                                return <li>
+                                        {parkCard.fullName}
+                                </li>
+                                })
+                            }
+                        </ul>
                 </div>
             </div>
         </>
