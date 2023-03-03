3aa5bbef7f8a85b9ab7f3493ed72685559c2b8b5
MariekeTeuben <marieke@marenmaa.nl>
2023-03-03 16:23:58 +0100

    setup favorite component

---
+++ src/components/favorite/favorite.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Favorite.css';
+
+function Favorite({image, title}) {
+
+    return(
+        <article className="favorite-container">
+            <img className="favorite-image" src={image} alt={title}/>
+            <h2 className="favorite-title">{title}</h2>
+        </article>
+    );
+}
+
+export default Favorite;
