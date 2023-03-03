0ecb7cce88d46cc9e382acef91da05eb63daf488
MariekeTeuben <marieke@marenmaa.nl>
2023-03-03 15:56:20 +0100

    get favorites from localStorage

--- src/pages/favorites/Favorites.js
+++ src/pages/favorites/Favorites.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './Favorites.css';
+import {Link} from "react-router-dom";
 
 function Favorites() {
+    const [favorites, setFavorites] = useState([]);
 
+
+    useEffect(() => {
+        function fetchFav() {
+            try {
+                const favorites = JSON.parse(localStorage.getItem('favorites'));
+                setFavorites([favorites]);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        fetchFav();
+    }, []);
+
+    console.log(favorites);
 
     return (
         <>
@@ -16,7 +33,18 @@ function Favorites() {
                 <section className="outer-content-container">
                     <div className="inner-content-container">
                         <h1>Favorites</h1>
-
+                        <div className="favorites-container">
+                            <ul>
+                                {favorites.length > 0 && favorites[0].map((favorite) => {
+                                    return <li key={favorite.id}>
+                                        <Link to={`parks/${favorite.id}`}>
+                                            {favorite.name}
+                                        </Link>
+                                    </li>
+                                })
+                                }
+                            </ul>
+                        </div>
                     </div>
                 </section>
             </main>
@@ -25,3 +53,4 @@ function Favorites() {
 }
 
 export default Favorites;
+
